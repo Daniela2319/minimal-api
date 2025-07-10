89f473823eed7bbd5f7577e05846990b3dd399b8
Daniela2319 <frankvt04@gmail.com>
2025-07-10 11:01:39 -0300

    Validação Login e Senha

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,10 +1,14 @@
 
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using minimal_api.Dominio.DTOs;
+using minimal_api.Dominio.Interfaces;
+using minimal_api.Dominio.Servicos;
 using minimal_api.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 
 builder.Services.AddDbContext<DbContexto>(options =>
 {
@@ -16,9 +20,9 @@ builder.Services.AddDbContext<DbContexto>(options =>
 var app = builder.Build();
 app.MapGet("/", () => "Good morning!");
 
-app.MapPost("/login", (LoginDTO loginDTO) =>
+app.MapPost("/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-  if (loginDTO.Email == "dani@test.com" && loginDTO.Senha == "senha123")
+  if (administradorServico.Login(loginDTO) != null)
     return Results.Ok("Login successful");
   else
     return Results.Unauthorized();
